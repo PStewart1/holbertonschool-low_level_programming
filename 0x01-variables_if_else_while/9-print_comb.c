4d00d90ade849617e26fc3f3e23eb7442572e5fb
PStewart1 <landofalcon007@gmail.com>
2022-05-23 10:09:04 -0700

    testing task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints each number from 0 to 9, using only putchar
+ *
+ * Return: 0, always succesful
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
