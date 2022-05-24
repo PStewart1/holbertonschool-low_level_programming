45f6397e18971112e829efd42d1455417111ecf8
PStewart1 <landofalcon007@gmail.com>
2022-05-24 16:06:29 -0500

    corrected description

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,7 @@
-/*#include <stdlib.h>
-#include <time.h>
-#include <stdio.h> */
 #include "main.h"
 
-/*
- * main - prints each letter from a to z
+/**
+ * main - Uses _putchar to iterate over a string
  *
  * Return: 0, always succesful
  */
@@ -14,11 +11,11 @@ int main(void)
 	char ch[] = "_putchar\n";
 	int i;
 
-        for (i = 0; i <= 8; i++)
-        {
-        	_putchar(ch[i]);
-        }
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(ch[i]);
+	}
 
-        return (0);
+	return (0);
 }
 
