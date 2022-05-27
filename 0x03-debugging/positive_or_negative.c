415c970732f598e44edd72b0ec4fe46f5154c156
PStewart1 <landofalcon007@gmail.com>
2022-05-27 09:40:27 -0500

    Wrote a file that tests "positive_or_negative" function for zero

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints whether random number is positive or negative
+ *
+ * Return: 0, always succesful
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+	return;
+}
