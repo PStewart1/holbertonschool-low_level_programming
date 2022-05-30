120968f8761304a158d4acbb058efac82d1d302a
PStewart1 <landofalcon007@gmail.com>
2022-05-29 21:26:22 -0500

    made > to >=

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -9,15 +9,41 @@
 int main(void)
 {
         int a, b, c;
-        int largest;
-
-        a = 972;
+        int largest, largestb, largestc;
+	
+	int i, j, k;
+	int x, y, z;
+        
+	a = 972;
         b = -98;
         c = 0;
+
+	x = 1;
+	y = 1;
+	z = 1;
+	i = 5;
+	j = 5;
+	k = 0;
 
         largest = largest_number(a, b, c);
 
         printf("%d is the largest number\n", largest);
+
+	a = 1;
+	b = 2;
+	c = 3;
+	
+	largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+	largestb = largest_number(i, j, k);
+
+        printf("%d is the largest number\n", largestb);	
+
+	largestc = largest_number(x, y, z);
+
+        printf("%d is the largest number\n", largestc);
 
         return (0);
 }
