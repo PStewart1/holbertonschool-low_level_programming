c786c3db9e0a715a3842f51135fae50411130264
PStewart1 <landofalcon007@gmail.com>
2022-06-01 17:26:24 -0500

    added while loop to find n, in order to include null

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, n = 0;
 
-	for (i = 0; src[i] != '\0'; ++i)
+	while (src[n] != '\0')
+		++n;
+
+	for (i = 0; i <= n; ++i)
 		dest[i] = src[i];
 
 	return (dest);
