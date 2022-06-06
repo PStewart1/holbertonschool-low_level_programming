5f0e46c40f345aaad3708a4fb89c6080ad18b23c
PStewart1 <landofalcon007@gmail.com>
2022-06-06 16:13:39 -0500

    added readme

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares 2 strings
+ *@s1: string to be compared
+ *@s2: string to be compared
+ *
+ * Return: 0 if equal, positve or negative int if unequal.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, n = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			n = s1[i] - s2[i];
+			return (n);
+		}
+		i++;
+	}
+
+
+	return (n);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# C - More pointers, arrays and strings - 0x06 
+## Description 
+#### Learning more about pointers, arrays and strings, and how to use them.
+ --- 
+## Author 
+#### -Paul Stewart
