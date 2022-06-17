ad412950e22a03b44fd035fdb452296018120d32
PStewart1 <landofalcon007@gmail.com>
2022-06-17 12:02:03 -0500

    Wrote a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

--- 0x0D-preprocessor/c.c
+++ 0x0D-preprocessor/c.c
@@ -1,5 +1,5 @@
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#include "4-sum.h"
+#include "4-sum.h"
 #include <stdio.h>
 
 /**
@@ -9,11 +9,9 @@
  */
 int main(void)
 {
-    int i;
-    int j;
+    int s;
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
+    s = SUM(98, 1024);
+    printf("%d\n", s);
     return (0);
 }
