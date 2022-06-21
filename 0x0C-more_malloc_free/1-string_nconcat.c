9e8658b54e795f6217d55fa184d03433abe32a25
PStewart1 <landofalcon007@gmail.com>
2022-06-21 12:57:21 -0500

    got rid of plus 1 in malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int x)
 	if (s2 == NULL)
 		s2 = "";
 
-	p = malloc(strlen(s1) + x + 1);
+	p = malloc(strlen(s1) + x);
 	if (p == NULL)
 		return (NULL);
 
