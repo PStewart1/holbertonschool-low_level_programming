179bb03cda77cdad91a519d53f5501dd8bd01465
PStewart1 <landofalcon007@gmail.com>
2022-06-22 16:52:08 -0500

    made pointer copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *name2 = name;
+	char *owner2 = owner;
 
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	d->name = name2;
 	d->age = age;
-	d->owner = owner;
+	d->owner = owner2;
 	return (d);
 }
