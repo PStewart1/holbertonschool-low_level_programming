7087fc74bbd51128553ec9311457a1d95f9cf130
PStewart1 <landofalcon007@gmail.com>
2022-06-22 14:52:10 -0500

    bettified and added ";" to end of struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct of dog
+ * @name: name of dog
+ * @owner: owner of dog
+ * @age: age of dog
+ *
+ * Description: a dog
+ */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-}
+};
 
 #endif /* DOG_H */
