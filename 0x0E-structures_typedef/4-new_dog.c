e227be79766d1f143df2efdc6219d1a322dd86ea
PStewart1 <landofalcon007@gmail.com>
2022-06-22 17:00:44 -0500

    malloced name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a struct dog with given parameters
@@ -13,13 +14,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *name2 = name;
-	char *owner2 = owner;
+	char *name2;
+	char *owner2;
 
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
 
+	name2 = malloc(sizeof(name));
+	if (name2 == NULL)
+		return (NULL);
+
+	owner2 = malloc(sizeof(owner));
+	if (owner2 == NULL)
+		return (NULL);
+
+	strcpy(name2, name);
+	strcpy(owner2, owner);
 	d->name = name2;
 	d->age = age;
 	d->owner = owner2;
