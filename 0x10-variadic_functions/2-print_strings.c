f5f6cb14f618e3cac104f62f0a84a98cd62e0df1
PStewart1 <landofalcon007@gmail.com>
2022-06-28 15:01:24 -0500

    Wrote a function that prints strings, followed by a new line.

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - variadic function  prints all int arguments
+ * @separator: separator between numbers tobe printed
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+	char *str = malloc(100);
+	char *sep = malloc(sizeof(separator) + 1);
+
+	if (n == 0)
+		return;
+
+	if (separator)
+		strcpy(sep, separator);
+	else
+		sep = "";
+
+	va_start(args, n);
+	for (i = 0; i < ((int)n - 1); i++)
+	{
+		strcpy(str, va_arg(args, char*));
+		if (str)
+			printf("%s%s", str, sep);
+		else
+			printf("(nil)%s", sep);
+	}
+	printf("%s\n", va_arg(args, char*));
+	va_end(args);
+	free(str);
+	free(sep);
+}
