f12f280de815862df313e3903da5cd76ce7692b9
PStewart1 <61563756+PStewart1@users.noreply.github.com>
2022-07-06 21:35:06 -0500

    Update variadic_functions.h
    added extra functions, and struct for function pointers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,23 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list c);
+void print_str(va_list s);
+void print_int(va_list i);
+void print_float(va_list i);
+void (*specifiercheck(const char *format))(va_list);
+
+/**
+ * struct print - structure for printing various types
+ * @spec: specifier char
+ * @f: print function
+ */
+typedef struct print
+{
+	char *spec;
+	void (*f)(va_list);
+} specifier;
+
 void print_all(const char * const format, ...);
 
 
