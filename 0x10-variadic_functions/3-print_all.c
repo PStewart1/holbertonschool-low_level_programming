2528d3adf480bcfd4c638afde995cea00a3b5b26
PStewart1 <61563756+PStewart1@users.noreply.github.com>
2022-07-06 21:38:07 -0500

    Update 3-print_all.c
    rewrote function to use new struct array of function pointers.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,9 +5,8 @@
 #include <stdarg.h>
 
 /**
- * print_strings - variadic function prints all string arguments
- * @separator: separator between strings to be printed
- * @n: number of arguments
+ * print_all - variadic function that prints multiple types
+ * @format: string that contains specifiers
  *
  * Return: void
  */
@@ -16,44 +15,25 @@ void print_all(const char *format, ...)
 {
 	va_list args;
 	int i = 0;
-	int skip;
-	char c;
-	int x;
-	float f;
-	char *str = malloc(100);
+	void (*fp)(va_list);
 
+    if(format == NULL)
+        return;
 	va_start(args, format);
-	while (format)
+	
+	while (format[i])
 	{
-		skip = 0;
-		switch (format[i]) {
-			case 'c':
-				c = (char)va_arg(args, int);
-				printf("%c", c);
-				break;
-			case 'i':
-				x = va_arg(args, int);
-				printf("%d", x);
-				break;
-			case 'f':
-				f = (float)va_arg(args, double);
-				printf("%f", f);
-				break;
-			case 's':
-				strcpy(str, va_arg(args, char*));
-				if (str)
-					printf("%s", str);
-				else
-					printf("(nil)");
-				break;
-			default :
-				skip = 1;
-			}
+		fp = specifiercheck(&format[i]);
+		if (!fp)
+		{
+		    i++;
+		    continue;
+		}
+		
+		fp(args);
 		i++;
-		if (format[i] && skip == 0)
-			printf(", ");
+		printf(", ");
 	}
 	printf("\n");
 	va_end(args);
-	free(str);
 }
