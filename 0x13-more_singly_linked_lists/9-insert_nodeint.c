6b5c00893e2993e303495d4099c77500d9169308
PStewart1 <landofalcon007@gmail.com>
2022-07-12 16:29:48 -0500

    added FREEs to failed checks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (*head == NULL || newnode == NULL)
+	{
+		free(newnode);
 		return (NULL);
-
+	}
 	node = *head;
 	for (i = 0; i < (idx - 1); i++)
 	{
@@ -24,7 +26,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		node = node->next;
 	}
 	if (i != idx - 1)
+	{
+		free(newnode);
 		return (NULL);
+	}
 	newnode->n = n;
 	newnode->next = node->next;
 	node->next = newnode;
