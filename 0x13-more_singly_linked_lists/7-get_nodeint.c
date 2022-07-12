3314f0483f474cd4d594e019abc389a211c52f0d
PStewart1 <landofalcon007@gmail.com>
2022-07-12 15:53:49 -0500

    put null check before node iteration

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,9 +18,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	node = head;
 	for (i = 0; i != index; i++)
 	{
-		node = node->next;
 		if (node->next == NULL)
 			return (NULL);
+		node = node->next;
 	}
 	return (node);
 }
