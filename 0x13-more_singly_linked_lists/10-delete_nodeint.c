33ea0c497277995ae8d1e3ab9ec82d244d0832fe
PStewart1 <landofalcon007@gmail.com>
2022-07-12 20:55:03 -0500

    removed * from null head check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node, *prev;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (1);
 
 	node = *head;
