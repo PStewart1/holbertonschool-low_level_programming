e62dc99ef4fe2a4a654b766644b3388138f521c3
PStewart1 <landofalcon007@gmail.com>
2022-07-18 20:56:25 -0500

    created new function for open(), in order to have more if checks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,39 @@
 #include "main.h"
 
 /**
+ * opener - a function that calls open() to open or create a file
+ * @filename: name of file to open
+ * @choice: selects whether to open existing file or create new file
+ * Return: file descriptor
+ */
+
+int opener(char *filename, int choice)
+{
+	int fd;
+
+	if (choice == 1)
+	{
+		fd = open(filename, O_RDWR);
+		if (fd == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", filename);
+			exit(98);
+		}
+	}
+	if (choice == 2)
+	{
+		umask(0002);
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0664);
+		if (fd == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", filename);
+			exit(99);
+		}
+	}
+	return (fd);
+}
+
+/**
  * main - a program that copies the content of a file to another file.
  * @ac: number of arguments passed
  * @av: array of arguments
@@ -19,17 +52,16 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	text[1024] = '\0';
-	fd1 = open(file_from, O_RDWR);
+	fd1 = opener(file_from, 1);
 	rdcount = read(fd1, text, 1024);
-	if (fd1 == -1 || rdcount == -1)
+	if (rdcount == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	umask(0002);
-	fd2 = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fd2 = opener(file_to, 2);
 	printed = write(fd2, text, rdcount);
-	if (printed < rdcount || fd2 == -1)
+	if (printed < rdcount)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
