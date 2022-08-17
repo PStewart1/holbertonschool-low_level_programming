d8bb49d6c1151966be1947a545bce3ae0fee923d
PStewart1 <landofalcon007@gmail.com>
2022-08-17 08:23:44 -0500

    removed "first", and changed last line

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,7 @@
 
 /**
  * hash_table_set - adds an element to a hash table.
+ * @ht: the hash table to update
  * @key: the key of the new element
  * @value: the value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
@@ -9,7 +10,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *head, *first, *node = malloc(sizeof(hash_node_t));
+	hash_node_t *head;
+	hash_node_t *node = (hash_node_t*)malloc(sizeof(hash_node_t));
 	unsigned long int index;
 
 	if (!key || strlen(key) <= 1 || !node )
@@ -19,13 +21,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!node->value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
+	node->next = NULL;
 	head = ht->array[index];
 	
-	if (head != NULL)
-	{
-		first = head;
-		node->next = first;
-	}
-	head = node;
+	if (head)
+		node->next = head;
+	ht->array[index] = node;
 	return (1);
 }
